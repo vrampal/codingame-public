4fbc21874c422759a09069bad91b0d5e2f8d8614
Vincent RAMPAL <vincent.rampal@gmail.com>
2017-08-17 22:06:36 +0200

    More Solutions

---
+++ src/main/java/training/community/gameOfLife/Solution.java
@@ -0,0 +1,83 @@
+package training.community.gameOfLife;
+
+import java.util.*;
+
+class Board {
+	final int width;
+	final int height;
+	final String[] rows;
+	
+	private Board(int width, int height) {
+		this.width = width;
+		this.height = height;
+		rows = new String[height];
+	}
+
+	Board(Scanner in) {
+		this(in.nextInt(), in.nextInt());
+		if (in.hasNextLine()) {
+			in.nextLine();
+		}
+		for (int rowIdx = 0; rowIdx < height; rowIdx++) {
+			rows[rowIdx] = in.next();
+		}
+	}
+	
+	boolean cellExist(int x, int y) {
+		return ((y >= 0) && (y < height) && (x >= 0) && (x < width));
+	}
+
+	private char getCell(int x, int y) {
+		return rows[y].charAt(x);
+	}
+
+	private int alive(int x, int y) {
+		int alive = 0;
+		if (cellExist(x, y)) {
+			alive = getCell(x, y) - '0';
+		}
+		return alive;
+	}
+	
+	char nextAlive(int x, int y) {
+		int neighbour = 0;
+		neighbour += alive(x    , y - 1);
+		neighbour += alive(x + 1, y - 1);
+		neighbour += alive(x + 1, y);
+		neighbour += alive(x + 1, y + 1);
+		neighbour += alive(x    , y + 1);
+		neighbour += alive(x - 1, y + 1);
+		neighbour += alive(x - 1, y);
+		neighbour += alive(x - 1, y - 1);
+		char nextAlive = getCell(x, y);
+		if ((nextAlive == '1') && (neighbour < 2) || (neighbour > 3)) {
+			nextAlive = '0';
+		} else if ((nextAlive == '0') && (neighbour == 3)) {
+			nextAlive = '1';
+		}
+		return nextAlive;
+	}
+}
+
+class Solution {
+
+    public static void main(String args[]) {
+    	new Solution().run();
+    }
+
+    Scanner in = new Scanner(System.in);
+    
+    void run() {
+    	Board board = new Board(in);
+    	StringBuilder builder = new StringBuilder();
+    	for (int y = 0; y < board.height; y++) {
+    		for (int x = 0; x < board.width; x++) {
+    			char ch = board.nextAlive(x, y);
+    			builder.append(ch);
+    		}
+    		builder.append('\n');
+    	}
+    	System.out.println(builder);
+    }
+
+}
---
+++ src/main/java/training/expert/theResistance/Solution.java
@@ -0,0 +1,134 @@
+package training.expert.theResistance;
+
+import java.util.*;
+
+class LexiNode {
+	LexiNode dot = null;
+	LexiNode dash = null;
+	int validWords = 0;
+
+	LexiNode getNode(char ch) {
+		if (ch == '.') return dot;
+		if (ch == '-') return dash;
+		throw new RuntimeException("Invalid character: " + ch);
+	}
+
+	LexiNode createNode(char ch) {
+		if (ch == '.') {
+			if (dot == null) {
+				dot = new LexiNode();
+			}
+			return dot;
+		}
+		if (ch == '-') {
+			if (dash == null) {
+				dash = new LexiNode();
+			}
+			return dash;
+		}
+		throw new RuntimeException("Invalid character: " + ch);
+	}
+}
+
+class LexiTree {
+	private final LexiNode root = new LexiNode();
+	
+	String encode(String word) {
+		StringBuilder builder = new StringBuilder();
+		for (int charIdx = 0; charIdx < word.length(); charIdx++) {
+			char ch = word.charAt(charIdx);
+			switch (ch) {
+			case 'A': builder.append(".-"); break;
+			case 'B': builder.append("-..."); break;
+			case 'C': builder.append("-.-."); break;
+			case 'D': builder.append("-.."); break;
+			case 'E': builder.append("."); break;
+			case 'F': builder.append("..-."); break;
+			case 'G': builder.append("--."); break;
+			case 'H': builder.append("...."); break;
+			case 'I': builder.append(".."); break;
+			case 'J': builder.append(".---"); break;
+			case 'K': builder.append("-.-"); break;
+			case 'L': builder.append(".-.."); break;
+			case 'M': builder.append("--"); break;
+			case 'N': builder.append("-."); break;
+			case 'O': builder.append("---"); break;
+			case 'P': builder.append(".--."); break;
+			case 'Q': builder.append("--.-"); break;
+			case 'R': builder.append(".-."); break;
+			case 'S': builder.append("..."); break;
+			case 'T': builder.append("-"); break;
+			case 'U': builder.append("..-"); break;
+			case 'V': builder.append("...-"); break;
+			case 'W': builder.append(".--"); break;
+			case 'X': builder.append("-..-"); break;
+			case 'Y': builder.append("-.--"); break;
+			case 'Z': builder.append("--.."); break;
+			default: throw new RuntimeException("Invalid character: " + ch);
+			}
+		}
+		return builder.toString();
+	}
+
+	void add(String word) {
+		String wordMorse = encode(word);
+		//System.err.println(word + " -> " + wordMorse);
+		LexiNode node = root;
+		for (int charIdx = 0; charIdx < wordMorse.length(); charIdx++) {
+			char ch = wordMorse.charAt(charIdx);
+			node = node.createNode(ch);
+		}
+		node.validWords++;
+	}
+
+	long decode(String encoded) {
+		long[] intermediate = new long[encoded.length()];
+		decode(encoded, intermediate, 0, 1);
+		for (int startIdx = 1; startIdx < encoded.length(); startIdx++) {
+			long base = intermediate[startIdx - 1];
+			if (base > 0) {
+				decode(encoded, intermediate, startIdx, base);
+			}
+		}
+		return intermediate[intermediate.length - 1];
+	}
+
+	private void decode(String encoded, long[] intermediate, int startIdx, long base) {
+		LexiNode node = root;
+		for (int charIdx = startIdx; charIdx < encoded.length(); charIdx++) {
+			char ch = encoded.charAt(charIdx);
+			node = node.getNode(ch);
+			if (node == null) {
+				return;
+			}
+			if (node.validWords > 0) {
+				intermediate[charIdx] += base * node.validWords;
+			}
+		}
+	}
+}
+
+class Solution {
+
+	public static void main(String args[]) {
+		new Solution().run();
+	}
+
+	Scanner in = new Scanner(System.in);
+
+	void run() {
+		LexiTree lexiTree = new LexiTree();
+		String encoded = in.next();
+		//System.err.println(encoded);
+
+		int wordCnt = in.nextInt();
+		for (int wordIdx = 0; wordIdx < wordCnt; wordIdx++) {
+			String word = in.next();
+			lexiTree.add(word);
+		}
+
+		long res = lexiTree.decode(encoded);
+		System.out.println(res);
+	}
+
+}
