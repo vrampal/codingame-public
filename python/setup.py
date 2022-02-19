5a6c2465f5be982a0a16cf3aa952e5bdb67d0dc7
Vincent RAMPAL <vincent.rampal@gmail.com>
2022-02-19 18:46:56 +0100

    Skeleton of python build

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='vrampal-codingame-public',
+    version='0.0.1',
+    author="Vincent RAMPAL",
+    author_email="vincent.rampal@gmail.com",
+    package_dir={"": "src"},
+)
